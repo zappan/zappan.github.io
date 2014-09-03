30b3ce269554a1c6d068858e5a1fd17bded18509
Dorijan Čovran <chorijan@gmail.com>
2014-09-03 19:21:16 +0200

    initial commit of scss file refactoring
    - made blocks of scss code (partials)
    - latest version of normalize

--- _site/compass/config.rb
+++ _site/compass/config.rb
@@ -1,10 +1,28 @@
+# -----------------------------------------------
+# Paths
+# -----------------------------------------------
 http_path    = "/" 
 project_path = "." 
-css_dir      = "output/assets/style" 
-sass_dir     = "content/assets/style" 
-images_dir   = "output/assets/images"
+css_dir      = "output/stylesheets/" 
+sass_dir     = "content/stylesheets/" 
+images_dir   = "output/images/"
 
 # when using SCSS:
 sass_options = {
   :syntax => :scss
 }
+
+# -----------------------------------------------
+# Output
+# -----------------------------------------------
+# Syntax preference
+preferred_syntax = :scss
+
+# Determine whether Compass generates relative or absolute paths
+relative_assets = false
+
+# Determines whether line comments should be added to compiled css for easier debugging
+line_comments  = false
+
+# CSS output style - :nested, :expanded, :compact, or :compressed
+output_style = :expanded
